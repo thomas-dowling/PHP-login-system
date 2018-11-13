a3d029fa779f6b3c803761c4bad7bb43fdd41244
Thomas Dowling <thomasdowling@Thomass-MacBook-Pro.local>
2018-11-13 14:51:02 +0000

    Add classes

--- dashboard.php
+++ dashboard.php
@@ -5,10 +5,15 @@
 	// Require the config
 	require_once "inc/config.php"; 
 
+	Page::ForceLogin();
+
+  $User = new User($_SESSION['user_id']);
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head> 
+  <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -23,14 +28,9 @@
   <body>
 
   	<div class="uk-section uk-container">
-  		<?php 
-  			echo "Hello world. Today is: ";
-  			echo date("Y m d");
-  		?> 
-  		<p>
-  			<a href="/login.php">Login</a>
-  			<a href="/register.php">Register</a>
-  		</p>
+  		<h2>Dashboard</h2>
+      <p>Hello <?php echo $User->email; ?>, you registered at <?php echo $User->reg_time; ?></p>
+      <p><a href="/logout.php">Logout</a></p>
   	</div>
 
   	<?php require_once "inc/footer.php"; ?> 
--- inc/config.php
+++ inc/config.php
@@ -5,12 +5,23 @@
 		exit('You do not have a config file');
 	}
 
+	// Sessions are always turned on
+	if(!isset($_SESSION)) {
+		session_start();
+	}
 
 	// Our config is below
-
+	// Allow errors
+	error_reporting(-1);
+	ini_set('display_errors', 'On');
+	
 
 	// Include the DB.php file;
 	include_once "classes/DB.php";
+	include_once "classes/Filter.php";
+	include_once "classes/Page.php";
+	include_once "classes/User.php";
+	include_once "functions.php";
 
 	$con = DB::getConnection();
 
